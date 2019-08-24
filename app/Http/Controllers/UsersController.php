d96a6e285578a977dbd555d67ef1886c32cd847d
Muzich <va7tal@mail.ru>
2019-08-24 11:08:25 +0500

    version 0.0.4

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -95,34 +95,39 @@ class UsersController extends Controller
 
             if(!is_null($end_date)) {
                 $database = FoodSelect::where(['user_id' => $id])->whereBetween('date', [$start_date, $end_date])->get();
-                $summaAll=0;$summa_z=0;$summa_o=0;$summa_u=0;
-                foreach ($database as $date){
-                    $sena=$this->getDayPrice($date->date);
-                    if($date->zavtrak==1){
-                        $summa_z+=$sena->zavtrak;
+                $summaAll = 0;
+                $summa_z = 0;
+                $summa_o = 0;
+                $summa_u = 0;
+                if(!empty($database)){
+                foreach ($database as $date) {
+                    $sena = $this->getDayPrice($date->date);
+                    if ($date->zavtrak == 1) {
+                        $summa_z += $sena->zavtrak;
                     }
 
-                    if($date->obed==1){
-                        $summa_o+=$sena->obed;
+                    if ($date->obed == 1) {
+                        $summa_o += $sena->obed;
                     }
 
-                    if($date->ujin==1){
-                        $summa_u+=$sena->ujin;
+                    if ($date->ujin == 1) {
+                        $summa_u += $sena->ujin;
                     }
-                    $sena=($date->zavtrak==1 ? $sena->zavtrak : 0) + ($date->obed==1 ? $sena->obed : 0) + ($date->ujin==1 ? $sena->ujin : 0);
-                    $summaAll=$summa_z+$summa_o+$summa_u;
+                    $sena = ($date->zavtrak == 1 ? $sena->zavtrak : 0) + ($date->obed == 1 ? $sena->obed : 0) + ($date->ujin == 1 ? $sena->ujin : 0);
+                    $summaAll = $summa_z + $summa_o + $summa_u;
                     //$v_ostatok=$v_ostatok-$summaAll;
-                    $array[]=[
-                        'date'=>$date->date,
-                        'balanceHistory'=>$balanceCurrent-$summaAll-$v_ostatok+$sena,
+                    $array[] = [
+                        'date' => $date->date,
+                        'balanceHistory' => $balanceCurrent - $summaAll - $v_ostatok + $sena,
                         //'currentBalance'=>$balanceCurrent-$summaAll,
                         //'summaBetween'=>$v_ostatok,
-                        'summa'=>round($sena,2),
+                        'summa' => round($sena, 2),
                         //x'balanceHistory'=>round($balanceHistory,2),
-                        'ostatok'=>round($balanceHistory-$summaAll,2),
+                        'ostatok' => round($balanceHistory - $summaAll, 2),
                     ];
                 }
-                $response=['reports'=>$array,'date_start'=>$start_date,'end_date'=>$end_date,'v_ostatok'=>$balanceCurrent-$eated_money,];
+            }
+                $response=['reports'=>!empty($array) ? $array : null,'date_start'=>$start_date,'end_date'=>$end_date,'v_ostatok'=>$balanceCurrent-$eated_money,];
             }
             return response($response,202);
         }else
