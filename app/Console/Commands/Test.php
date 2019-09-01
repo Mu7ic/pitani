865efd0dcd025e2f9da22d7c2389e597ec23a80f
Muzich <va7tal@mail.ru>
2019-09-01 21:48:03 +0500

    version 0.0.5

---
+++ app/Console/Commands/Test.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Foundation\Inspiring;
+use App\Users;
+use App\FoodSelect;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'default';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Setting food_date by default';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $date = date('Y-m-d', strtotime(date('') . ' +1 day'));
+        $select=Users::where('isAdmin',0)->where('isActive',1)->get();
+
+        foreach ($select as $user){
+            $food=FoodSelect::where(['user_id'=>$user->id,'date'=>$date])->first();
+            if(empty($food)){
+                $foodSelect= new FoodSelect();
+                $foodSelect->date=$date;
+                $foodSelect->user_id=$user->id;
+                $foodSelect->zavtrak=1;
+                $foodSelect->obed=1;
+                $foodSelect->ujin=0;
+                $foodSelect->created_at=date('Y-m-d H:i:s');
+                $foodSelect->updated_at=date('Y-m-d H:i:s');
+                $foodSelect->save();
+                $this->comment($user->id.PHP_EOL);
+            }
+        }
+
+
+    }
+}
